78a32b0dd158efd7b7883e889551eec7c3dedf8e
7erra <ichbinterra@gmail.com>
2025-10-01 13:59:24 +0200

    Download files in main

--- src/constants/urls.py
+++ src/constants/urls.py
@@ -1,1 +1,2 @@
-URL_GDS_DISTRICTS = "https://gds.hessen.de/INTERSHOP/rest/WFS/HLBG-Geodaten-Site/-/downloadcenter?path=3D-Daten%2FDigitales+Gel%C3%A4ndemodell+%28DGM1%29"
+URL_GDS_BASE = "https://gds.hessen.de"
+URL_GDS_DISTRICTS = f"{URL_GDS_BASE}/INTERSHOP/rest/WFS/HLBG-Geodaten-Site/-/downloadcenter?path=3D-Daten%2FDigitales+Gel%C3%A4ndemodell+%28DGM1%29"
--- src/gds_hessen_downloader.py
+++ src/gds_hessen_downloader.py
@@ -1,5 +1,20 @@
+import sys
 from gds.districts import get_districts
+from download import download
+from gds.muncipalities import get_muncipalities
+from constants.urls import URL_GDS_BASE
+from urllib.parse import quote
 
 
 if __name__ == "__main__":
-    print(get_districts())
+    _, download_dir = sys.argv
+    for district in get_districts():
+        district_print_message = f"### Downloading district {district.name} ###"
+        print("#" * len(district_print_message))
+        print(district_print_message)
+        print("#" * len(district_print_message))
+        for muncipality in get_muncipalities(f"{URL_GDS_BASE}/{district.uri}"):
+            download(
+                f"{URL_GDS_BASE}{quote(muncipality.uri)}",
+                f"{download_dir}/{muncipality.name}.zip",
+            )
