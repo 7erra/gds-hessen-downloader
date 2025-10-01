6163faec17688956b2513369a42718ca2b53ceb5
7erra <ichbinterra@gmail.com>
2025-10-01 02:13:27 +0200

    Return list of URIs

--- src/gds/districts.py
+++ src/gds/districts.py
@@ -4,4 +4,4 @@ from constants.urls import URL_GDS_DISTRICTS
 
 
 def get_districts():
-    return requests.get(URL_GDS_DISTRICTS).json()
+    return [x["uri"] for x in requests.get(URL_GDS_DISTRICTS).json()["navigation"]]
--- tests/gds/test_districts.py
+++ tests/gds/test_districts.py
@@ -1,11 +1,15 @@
 from constants.urls import URL_GDS_DISTRICTS
 from gds.districts import get_districts
+from ..data.gds import test_gds_districts_response
 
 
 def test_get_districts(requests_mock):
-    response = {"test": "ok"}
+    response = test_gds_districts_response
     requests_mock.get(URL_GDS_DISTRICTS, json=response)
 
     result = get_districts()
 
-    assert result == response
+    assert result == [
+        "/INTERSHOP/rest/WFS/HLBG-Geodaten-Site/-/downloadcenter?path=3D-Daten%2FDigitales+Gel%C3%A4ndemodell+%28DGM1%29%2FHochtaunuskreis",
+        "/INTERSHOP/rest/WFS/HLBG-Geodaten-Site/-/downloadcenter?path=3D-Daten%2FDigitales+Gel%C3%A4ndemodell+%28DGM1%29%2FLahn-Dill-Kreis",
+    ]
